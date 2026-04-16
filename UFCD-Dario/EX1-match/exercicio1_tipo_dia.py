3b047f66dcb3fc62eef45dc1e9babb4cc527d8ad
GabrielSimao89 <gabriel.simao.t0129860@edu.atec.pt>
2026-04-16 16:38:26 +0100

    Delete UFCD-Dario/EX1-match directory

--- UFCD-Dario/EX1-match/exercicio1_tipo_dia.py
+++
@@ -1,9 +0,0 @@
-dia = input("Introduz um dia da semana: ").strip().lower()
-
-match dia:
-    case "segunda" | "terça" | "terca" | "quarta" | "quinta" | "sexta":
-        print("Dia útil")
-    case "sábado" | "sabado" | "domingo":
-        print("Fim de semana")
-    case _:
-        print("Dia inválido")
